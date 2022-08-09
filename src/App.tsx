de2edfed1843262542371f53b6d16c68bbf9ac0e
larissakarla <47679838+larissakarla@users.noreply.github.com>
2022-08-08 21:10:51 -0300

    Update App.tsx
    Adicionado código para acessar API

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function App() {
-	const [count, setCount] = useState(0);
+const App = (props: any) => {
+	const [users, setUsers] = useState([
+		{ id: 1, name: "Minora" },
+		{ id: 2, name: "Ataide" },
+	]);
+  const [loading, setLoading] = useState(true);
 
-	return <div>Lista de usuários</div>;
-}
+	useEffect(() => {
+		fetch("https://jsonplaceholder.typicode.com/users/")
+			.then((response) => response.json())
+			.then((json) => {setUsers(json); setLoading(false)});
+	});
+	return (
+		<div className="App">
+			<h1>Lista de usuários</h1>
+			<div className="card">
+        {loading ? <h2>Carregando...</h2> : null}
+				<ul>
+					{users.map((user) => (
+						<li key={user.id}>{user.name}</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
+};
 
 export default App;
 
